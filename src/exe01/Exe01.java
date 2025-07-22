56dee7b8dbe7dac025ac4606f98c0af3b5d52ced
Marcos Paulo <marcospaulomachado331@gmail.com>
2025-07-22 16:22:21 -0300

    Estrutura condicional

---
+++ src/exe01/Exe01.java
@@ -0,0 +1,25 @@
+package exe01;
+
+import java.util.Scanner;
+
+public class Exe01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		num = sc.nextInt();
+		
+		if(num < 0 ) {
+			System.out.println("NEGATIVO");
+		} else {
+			System.out.println("NÃO NEGATIVO ");
+		}
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/exe03/Exe03.java
@@ -0,0 +1,26 @@
+package exe03;
+
+import java.util.Scanner;
+
+public class Exe03 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B;
+		
+	    A = sc.nextInt();
+	    B = sc.nextInt();
+	    
+	    if(A % B == 0 || B % A == 0) {
+	    	System.out.println("Sao Multiplos");
+	    } else {
+	    	System.out.println("Nao sao Multiplos");
+	    }
+	    
+		
+		sc.close();
+	}
+
+}
